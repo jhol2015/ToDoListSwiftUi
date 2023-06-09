f2677647d617a61f4d1f9bdafc59d15ab002b3ae
Jhol Moreira Mendanha <galilleu2011@gmail.com>
2023-06-09 17:40:17 -0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jhol Moreira on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
