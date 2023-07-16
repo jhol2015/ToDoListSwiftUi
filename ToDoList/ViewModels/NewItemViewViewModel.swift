92405b2ec121fc7e22c205ee1e8587be5c67fe51
Jhol Moreira Mendanha <galilleu2011@gmail.com>
2023-07-16 19:45:42 -0300

    ajuste na inclusão de data

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -10,6 +10,8 @@ import Foundation
 class NewItemViewViewModel: ObservableObject {
     @Published var title = ""
     @Published var dueDate = Date()
+    @Published var showAlert = false
+
     
     init() {
         
@@ -17,5 +19,15 @@ class NewItemViewViewModel: ObservableObject {
     
     func save() {
         
+    }
+    
+    var canSave: Bool {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        guard dueDate >= Date().addingTimeInterval(-86400) else {
+            return false
+        }
+        return true
     }
 }
